872d5d2464476e32b1b83b5514db3e2eaa12b9d1
XenHat <XenHat@users.noreply.github.com>
2018-02-14 18:38:13 -0500

    Build using VS2017 when VS2015 is not present

--- creator_win64.bat
+++ creator_win64.bat
@@ -1,4 +1,15 @@
-call "C:\Program Files (x86)\Microsoft Visual studio 14.0\VC\vcvarsall.bat" amd64
+@echo off
+rem :: VS2015
+IF EXIST "C:\Program Files (x86)\Microsoft Visual studio 14.0\VC\vcvarsall.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual studio 14.0\VC\vcvarsall.bat" amd64
+) else (
+    rem :: VS2017
+    rem :: NOTE: The instructions contained in the NMakefile are no longer necessary with this method.
+    rem :: Simply run the script, as long as C:\flexandbison exists.
+    pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\"
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+    popd
+)
 set PATH=%PATH%;C:\flexandbison
 nmake /F NMakefile
 mkdir binary\windows64\
